636e9f167a4b222da1f19415a870d9153e3e46f1
Arjun Lalwani <arjunlalwani@D-10-157-119-154.dhcp4.washington.edu>
2016-10-29 15:24:52 -0700

    new files

---
+++ SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  adv-calc
+//
+//  Created by Arjun Lalwani on 29/10/16.
+//  Copyright © 2016 Arjun Lalwani. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var historyLabel: UILabel!
+    
+    var history: String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        historyLabel.text = history
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ adv-calc/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  adv-calc
+//
+//  Created by Arjun Lalwani on 29/10/16.
+//  Copyright © 2016 Arjun Lalwani. All rights reserved.
+//
+
+import Foundation
