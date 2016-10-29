983ddbb5d2e14a6385e34be10e99e9ceb73ff78a
Arjun Lalwani <arjunlalwani@D-10-157-119-154.dhcp4.washington.edu>
2016-10-29 12:49:19 -0700

    Add new files

---
+++ adv-calc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  adv-calc
+//
+//  Created by Arjun Lalwani on 28/10/16.
+//  Copyright © 2016 Arjun Lalwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
